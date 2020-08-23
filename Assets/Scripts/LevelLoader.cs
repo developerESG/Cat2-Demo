3f6a8c3739024000ba7e9e2fe8c08d3f8fde0142
Feizen <msifeizen@gmail.com>
2020-08-23 20:28:22 +0300

    2 commit Juri
    - Loading screen remade
    - Fixed bad behaviour in android build

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -8,23 +8,36 @@ public class LevelLoader : MonoBehaviour
 {
     public GameObject loadingSrceen;
     public Slider slider;
+    private float Timer = 0f;
 
     void Start()
     {
-        StartCoroutine(LoadAsynchroniously(1));
+        //StartCoroutine(LoadAsynchroniously(1));
+    }
+
+    void Update()
+    {
+        Timer += Time.deltaTime;
+        slider.value = Timer;
+        if (Timer >= 1f) { 
+            Debug.Log("scene loaded");
+            SceneManager.LoadScene(1);
+        }
+
     }
     IEnumerator LoadAsynchroniously(int sceneIndex)
     {
         AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
 
         loadingSrceen.SetActive(true);
-        while (operation.isDone == false)
-        {
-            //yield return new WaitForSeconds(10f);
-            float progress = Mathf.Clamp01(operation.progress / .9f);
-            slider.value = progress;
 
-            yield return null;//new WaitForSeconds(3);
+            while (operation.isDone == false)
+            {
+                float progress = Mathf.Clamp01(operation.progress / .9f);
+                slider.value = progress;
+            yield return null;
+            }
         }
     }
-}
+
+
