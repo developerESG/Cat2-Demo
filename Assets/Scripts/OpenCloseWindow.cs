2efe0d668995a095d37deeb5d3560c25cf291cbc
Diana Morozova <diana@fullxp.consulting>
2020-09-21 13:37:52 +0300

    NewLevelofFame popup fixed

--- Assets/Scripts/OpenCloseWindow.cs
+++ Assets/Scripts/OpenCloseWindow.cs
@@ -7,6 +7,12 @@ public class OpenCloseWindow : MonoBehaviour
 
     public GameObject window;
     [SerializeField] GameObject newLVL;
+    public bool newLvLOnce;
+
+    private void Start()
+    {
+        newLvLOnce = true;
+    }
 
     public void openWindow()
     {
@@ -32,11 +38,14 @@ public class OpenCloseWindow : MonoBehaviour
 
     public void NewLevel()
     {
-        if(PartyStarted.partyDone == "y")
+        
+        if(PartyStarted.partyDone == "y" && newLvLOnce == true)
         {
             newLVL.SetActive(true);
+            newLvLOnce = false;
+           
         }
     }
-
+    
 
 }
