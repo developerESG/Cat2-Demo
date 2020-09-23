2a784e5af0ad990fcddb4f3fda6031f535fc0b1f
Diana Morozova <diana@fullxp.consulting>
2020-09-23 18:43:28 +0300

    Pusations sequence and deactivation fix

--- Assets/Scripts/IconsClick.cs
+++ Assets/Scripts/IconsClick.cs
@@ -1,11 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class IconsClick : MonoBehaviour
 {
     [SerializeField] private Animator petstagIcon,partyIcon,guestbook, furniture;
+    [SerializeField] private GameObject newItems;
+    [SerializeField] private Button guestBookIcon;
+    trigger triggerScript;
 
     // Start is called before the first frame update
     private void Awake()
@@ -15,25 +19,33 @@ public class IconsClick : MonoBehaviour
 
     void Start()
     {
+        guestBookIcon = GetComponent<Button>();
        
         partyIcon.SetBool("noPulse", true);
         guestbook.SetBool("noPulse", true);
         furniture.SetBool("noPulse", true);
     }
 
+   
     // Update is called once per frame
     void Update()
     {
-       
 
-        if (Input.GetMouseButtonDown(0))
+       if(PartyStarted.partyDone == "y")
         {
-            petstagIcon.SetBool("noPulse", true);
             partyIcon.SetBool("noPulse", true);
-            guestbook.SetBool("noPulse", true);
+            petstagIcon.SetBool("noPulse", true);
+            //guestbook.SetBool("noPulse", true);
             furniture.SetBool("noPulse", true);
+
+            if (!newItems.activeInHierarchy)
+            {
+                //guestbook.SetBool("noPulse", false);
+                
+            }
+
         }
-
+       
     }
-
+    
 }
